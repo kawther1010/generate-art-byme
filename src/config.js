47e24740cd291ef4047ffc3d48e3dcf473570302
kawther <kther54O@gmail.com>
2022-03-28 02:55:02 +0100

    to try over a new data just modify the config.js file and change the amount of folders and imgs and press node app.js, and chill XD

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+    { name: 'face', number: 3 },
+    { name: 'hair', number: 3 },
 ];
   
 const format = {
@@ -20,6 +14,6 @@ const rarity = [
     { key: "_sr", val: "super rare" },
 ];
 
-const defaultEdition = 5;
+const defaultEdition = 9;
 
 module.exports = { layersOrder, format, rarity, defaultEdition };
